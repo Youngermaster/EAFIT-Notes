2adf5df31ae6bf17645296a7d0f7818c8762a3d8
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2023-12-17 09:08:54 -0500

    💀 Backup

--- LaTex/12th-Semester/NumericalAnalysis/Parcial_3/jacobi-6-3.py
+++ LaTex/12th-Semester/NumericalAnalysis/Parcial_3/jacobi-6-3.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def jacobi(A, b, x0, tol, max_iterations=100):
     n = A.shape[0]
@@ -8,21 +9,37 @@ def jacobi(A, b, x0, tol, max_iterations=100):
 
     for k in range(max_iterations):
         x_new = (b - np.dot(R, x)) / D
-        if np.linalg.norm(x_new - x, np.inf) / np.linalg.norm(x_new, np.inf) < tol:
+        if np.linalg.norm(x_new - x, ord=np.inf) / np.linalg.norm(x_new, ord=np.inf) < tol:
             return x_new, k+1  # Return the solution and the number of iterations
         x = x_new
 
     return x, k+1  # If tolerance is not reached, return the last iteration
 
-# Set the value of x for the location, assuming c is replaced by 0
-x_values = [1.5]  # The x values for the locations mentioned
-tol = 1e-2  # Tolerance of two significant figures
+# Define the range of x values
+x_values = np.linspace(1, 10, 100)  # Let's say we are interested in values from 1 to 10
+iterations_list = []
 
+# Tolerance of two significant figures
+tol = 1e-2
+
+# Run the Jacobi method for each x value
 for x_value in x_values:
     A = np.array([[11 - x_value, 5, 6], [-2, 4, 1], [-1, -1, 4]])
     b = np.array([15, 15, 20])
     x0 = np.array([1, 1, 1])
     
-    # Run the Jacobi method
-    solution, iterations = jacobi(A, b, x0, tol)
-    print(f"La solución para x={x_value} es {solution} y el número de iteraciones es {iterations}")
+    try:
+        _, iterations = jacobi(A, b, x0, tol)
+        iterations_list.append(iterations)
+    except Exception as e:
+        # In case of an error, append a None or some indicator to signify the failure
+        iterations_list.append(None)
+        print(f"Jacobi method failed for x={x_value} with error: {e}")
+
+# Plotting
+plt.plot(x_values, iterations_list, marker='o')
+plt.xlabel('x value')
+plt.ylabel('Number of Iterations')
+plt.title('Jacobi Method Convergence')
+plt.grid(True)
+plt.show()
