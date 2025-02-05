1d1efd0f947bcc852508afa277d13b680e53f1a3
Juan Manuel Young Hoyos <vndrzrfshJXY@wegmans.com>
2025-02-05 09:56:09 -0500

    Quizz examples

---
+++ LaTex/15th-Semester/NumericalAnalysis/Quizz1/example.py
@@ -0,0 +1,15 @@
+# Valor exacto de x:
+x = 7/13
+print("Valor exacto de x:", x)
+
+# Aproximación con 13 cifras significativas:
+x_aprox = 0.5384615384615
+print("Aproximación con 13 cifras significativas:", x_aprox)
+
+# Error absoluto:
+error_absoluto = abs(x - x_aprox)
+print("Error absoluto:", error_absoluto)
+
+# Error relativo:
+error_relativo = error_absoluto / abs(x)
+print("Error relativo:", error_relativo)
