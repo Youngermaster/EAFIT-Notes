1f5a92011cd11099e387f83763a87aee1d8c68a9
Juan Manuel Young Hoyos <vndrzrfshJXY@wegmans.com>
2025-02-05 11:19:28 -0500

    Some examples added

---
+++ LaTex/15th-Semester/NumericalAnalysis/Quizz1/example_2.py
@@ -0,0 +1,15 @@
+# Valor exacto de x:
+x = 3/22
+print("Valor exacto de x:", x)
+
+# Aproximación con 8 decimales correctos:
+x_aprox = 0.13636369
+print("Aproximación con 8 decimales correctos:", x_aprox)
+
+# Error absoluto:
+error_absoluto = abs(x - x_aprox)
+print("Error absoluto:", error_absoluto)
+
+# Error relativo:
+error_relativo = error_absoluto / abs(x)
+print("Error relativo:", error_relativo)
